2d9477c299dcf0c5e6bbba5c4190dfa612955447
mithiladhanundara <mithiladhanundara@gmail.com>
2025-10-23 19:39:58 +0530

    Remove temporary test file

--- src/app/admin/page-test.js
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-import Link from 'next/link';
-
-// Simple test version without KV dependency
-export default async function AdminDashboard() {
-  // For testing, always redirect to login
-  redirect('/admin/login');
-}
